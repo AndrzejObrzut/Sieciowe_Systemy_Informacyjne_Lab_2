f56036a7ab7abe0cdd078de2248e0406bbd8cd62
AndrzejObrzut <andrzejo94@gmail.com>
2016-11-03 19:55:05 +0100

    add action bean

---
+++ SSI2/src/java/com/myapp/struts/EnterActionFormBean.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myapp.struts;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class EnterActionFormBean extends org.apache.struts.action.ActionForm {
+    
+    private String name;   
+    private int number;
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    private String surname;
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param string
+     */
+    public void setName(String string) {
+        name = string;
+    }
+
+    /**
+     * @return
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param i
+     */
+    public void setNumber(int i) {
+        number = i;
+    }
+
+    /**
+     *
+     */
+    public EnterActionFormBean() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (getName() == null || getName().length() < 1) {
+            errors.add("name", new ActionMessage("error.name.required"));
+            // TODO: add 'error.name.required' key to your resources
+        }
+        return errors;
+    }
+}
